222a2599a2e0229d0f9c09e7aeab4a9922be5e73
Simran Thind <simran.thind@outlook.com>
2022-10-17 16:15:29 -0400

    roll back URLs

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from app import views
 from django.urls import path
 
+app_name = 'app'
+
 urlpatterns = [
     path('create/', views.create_recipe, name='create-recipe'),
     path('list/', views.RecipeListView.as_view(), name='view-recipes'),
