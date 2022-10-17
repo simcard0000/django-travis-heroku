696447aec148f2279a3d27d53df29c280a8c66e4
Simran Thind <simran.thind@outlook.com>
2022-10-17 15:51:57 -0400

    fix urls.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include("app.urls"))] #route for new app
+    path('app/', include(('app.urls', 'app'), namespace='app'))] #route for new app
 
